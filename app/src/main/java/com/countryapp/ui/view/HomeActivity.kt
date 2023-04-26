0d641c1423d2760d5ffbaeb7719da015f696ed0c
Miguel <miguel.cano.de.sola@gmail.com>
2023-04-26 10:30:55 +0200

    Clean architecture

--- app/src/main/java/com/countryapp/ui/view/HomeActivity.kt
+++ app/src/main/java/com/countryapp/ui/view/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.countryapp
+package com.countryapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.countryapp.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
