fd8f6fd3dbb1d97963b5b913c0584db19572fb4e
Miguel <miguel.cano.de.sola@gmail.com>
2023-04-28 13:13:47 +0200

    Load info en detailFragment

--- app/src/main/java/com/countryapp/ui/view/search/fragment/DetailFragment.kt
+++ app/src/main/java/com/countryapp/ui/view/search/fragment/DetailFragment.kt
@@ -6,35 +6,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.countryapp.databinding.FragmentDetailBinding
-import com.countryapp.ui.view.HomeActivity
+import com.countryapp.ui.domain.model.DetailCountryItem
+import com.countryapp.ui.view.search.viewmodel.DetailViewModel
+import com.squareup.picasso.Picasso
+import dagger.hilt.android.AndroidEntryPoint
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
+    private var id: String? = null
+    private val detailViewModel: DetailViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        initUI()
+        arguments?.let {
+            id = it.getString("ID")
+        }
 
-    }
+        Log.i("Milito", id.toString())
+        if(id!=null){
 
-    private fun initUI() {
-        val activity = activity as HomeActivity
-        activity.binding.navBar.setOnClickListener{
-            Log.i("Milito", "Adios")
+            detailViewModel.getCountry(id.toString())
+
+            detailViewModel.countryInfo.observe(viewLifecycleOwner, Observer {
+                updateView(it)
+            })
         }
     }
+
+    private fun updateView(country: DetailCountryItem) {
+        if(country.flags!= null)
+            Picasso.get().load(country.flags.png).into(binding.ivDetailCountry)
+        binding.tvDetailTittle.text = country.name.common
+        binding.tvDetailCapital.text = country.capital!![0]
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
     }
--- app/src/main/java/com/countryapp/ui/view/search/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/countryapp/ui/view/search/viewmodel/DetailViewModel.kt
@@ -1,4 +1,26 @@
 package com.countryapp.ui.view.search.viewmodel
 
-class DetailViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.countryapp.ui.domain.GetCountryByID
+import com.countryapp.ui.domain.model.DetailCountryItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val getCountriesByID: GetCountryByID
+) : ViewModel() {
+    val countryInfo = MutableLiveData<DetailCountryItem>()
+
+    fun getCountry(id: String){
+        viewModelScope.launch {
+            val result = getCountriesByID(id)
+            Log.i("Milito", result[0].toString())
+            countryInfo.postValue(result[0])
+        }
+    }
 }
